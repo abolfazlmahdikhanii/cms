06a07d3228db9fd399e8c6f641a192c119939ad4
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-03-09 21:12:43 +0330

    modifie post component

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./post.css";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { supabase } from "../../superbase";
 import useRelativeTime from "../../hooks/useRelativeTime";
 import Box from "../../components/Ui/Box/Box";
 import useTranslatorCategory from "../../hooks/useTranslatetorCategory";
+import Aside from "../Aside/Aside";
 
 
 const Post = ({ blogs }) => {
@@ -14,7 +15,7 @@ const Post = ({ blogs }) => {
 
     const match = useParams();
 
-    const timeFormat=useRelativeTime
+    const timeFormat = useRelativeTime;
     const translator = useTranslatorCategory;
     useEffect(() => {
         getBlogData();
@@ -65,7 +66,7 @@ const Post = ({ blogs }) => {
 
         setContent(data);
 
-   
+
 
 
     };
@@ -130,7 +131,61 @@ const Post = ({ blogs }) => {
             </main>
 
             <aside className="blog-aside">
+                <div className="register-box">
+                    <h4 className="register-box__title">در دیجی بلاگ ثبت نام کنید</h4>
+                    <p className="register-box__dis">جهت بهره‌مندی و دسترسی به امکانات ویژه و بخش‌های مختلف در دیجی بلاگ عضو ویژه دیجی بلاگ شوید.</p>
+                    <Link to="/auth" className="btn-action btn-item btn-link">
+                        عضویت در دیجی بلاگ
+                    </Link>
+                </div>
 
+                <Box>
+                    <div className="author-profile">
+                        <div className="author-profile--photo">
+                            <img src={'../../../src/assets/profile.svg'} alt="" />
+                        </div>
+                        <div className="author-profile--info">
+                            <p className="author-prfile__fullName">
+                                {`${blogContent[0]?.post_author.firstName} ${blogContent[0]?.post_author.lastName}`}
+                            </p>
+                            <p className="author-prfile__dis"></p>
+                            <button className="btn-follow">دنبال کردن</button>
+                        </div>
+                    </div>
+                </Box>
+
+                <Aside type="viewst" />
+                <Box>
+                    <div className="share-post">
+                        <p>به اشتراک بگذارید</p>
+                        <div className="social">
+                            <p>
+                                <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <circle cx="12" cy="12" r="12" fill="#1DA1F2"></circle>
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M17.5675 8.98649C17.5728 9.11843 17.5745 9.25031 17.5745 9.38224C17.5745 13.3822 14.6857 18 9.40302 18C7.78027 18 6.27135 17.4962 5 16.6387C5.22473 16.6627 5.45297 16.6807 5.68471 16.6807C7.03019 16.6807 8.26943 16.1948 9.25242 15.3792C7.99566 15.3613 6.93446 14.4797 6.56847 13.2803C6.74417 13.3163 6.92511 13.3343 7.11015 13.3343C7.37107 13.3343 7.62441 13.2984 7.86724 13.2264C6.55211 12.9506 5.56154 11.7272 5.56154 10.2579C5.56154 10.2399 5.56154 10.2339 5.56154 10.2219C5.94914 10.4438 6.39276 10.5817 6.86382 10.5997C6.09214 10.0539 5.58488 9.12441 5.58488 8.07494C5.58488 7.52321 5.72614 7.00145 5.97481 6.55168C7.39092 8.38676 9.50809 9.59217 11.8949 9.71811C11.8459 9.49622 11.8208 9.2624 11.8208 9.02851C11.8208 7.35535 13.1067 6 14.6933 6C15.5193 6 16.2652 6.36587 16.7888 6.95358C17.4444 6.82164 18.0584 6.56979 18.6142 6.22196C18.3988 6.92961 17.944 7.52318 17.3498 7.895C17.9312 7.82303 18.4857 7.66124 19 7.42136C18.6142 8.02706 18.1285 8.56071 17.5675 8.98649Z" fill="white"></path>
+                                </svg>
+                            </p>
+                            <p>
+                                <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M0.512309 11.8958C0.511586 13.9188 1.04259 15.8938 2.05227 17.6349L0.415588 23.5836L6.53091 21.9874C8.21581 22.9014 10.1128 23.3839 12.0434 23.3845H12.0484C18.4062 23.3845 23.5812 18.2344 23.584 11.9048C23.5852 8.83736 22.3863 5.95342 20.2082 3.78333C18.0304 1.61354 15.1342 0.417944 12.0479 0.416504C5.68976 0.416504 0.514777 5.5659 0.512021 11.8958L0.512309 11.8958ZM12.0483 23.3844H12.0484H12.0483C12.048 23.3844 12.0481 23.3844 12.0483 23.3844Z" fill="url(#paint0_linear_66_2179)"></path>
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M9.06359 6.92064C8.83989 6.42566 8.60451 6.41575 8.39175 6.40704C8.21762 6.39967 8.01847 6.40015 7.81955 6.40015C7.62039 6.40015 7.29692 6.47454 7.02339 6.77197C6.74962 7.06942 5.97827 7.78841 5.97827 9.2507C5.97827 10.7131 7.04833 12.1262 7.19741 12.3247C7.34675 12.5228 9.26303 15.6197 12.2976 16.8109C14.8199 17.801 15.3332 17.6041 15.8806 17.5545C16.428 17.505 17.6472 16.8357 17.8959 16.1417C18.1447 15.4477 18.1447 14.8528 18.0701 14.7286C17.9955 14.6047 17.7964 14.5303 17.4978 14.3818C17.1991 14.2332 15.7312 13.5141 15.4576 13.4149C15.1838 13.3158 14.9848 13.2663 14.7856 13.5639C14.5866 13.8611 14.0147 14.5304 13.8405 14.7286C13.6664 14.9272 13.4921 14.952 13.1935 14.8034C12.8949 14.6542 11.9332 14.3407 10.7924 13.3283C9.90479 12.5404 9.30559 11.5676 9.13134 11.27C8.95721 10.9728 9.11275 10.8118 9.26245 10.6636C9.3966 10.5304 9.56113 10.3166 9.71047 10.1431C9.85945 9.96951 9.90915 9.84565 10.0087 9.64739C10.1083 9.44901 10.0585 9.27543 9.98388 9.12676C9.90915 8.9781 9.32891 7.50829 9.06341 6.92055" fill="white"></path>
+                                    <defs>
+                                        <linearGradient id="paint0_linear_66_2179" x1="11.9999" y1="23.5812" x2="11.9999" y2="0.414069" gradientUnits="userSpaceOnUse">
+                                            <stop stop-color="#20B038"></stop>
+                                            <stop offset="1" stop-color="#60D66A"></stop>
+                                        </linearGradient>
+                                    </defs>
+                                </svg>
+                            </p>
+                            <p>
+                                <svg width="20" height="20" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <circle cx="12.9996" cy="13" r="12.5" fill="#0088CC" stroke="white"></circle>
+                                    <path d="M18.987 7.08265L5.70001 12.1745C4.79315 12.5361 4.79853 13.0386 5.53471 13.2627L8.84854 14.2904L10.1165 18.1528C10.2706 18.5755 10.1946 18.7432 10.6414 18.7432C10.9862 18.7432 11.1392 18.587 11.3312 18.4006C11.4534 18.2819 12.1784 17.5815 12.9881 16.7994L16.4351 19.3298C17.0693 19.6775 17.5273 19.4974 17.6853 18.7445L19.948 8.15037C20.1796 7.22757 19.5939 6.80899 18.987 7.08265ZM9.3686 14.0543L16.838 9.37207C17.2109 9.14739 17.5528 9.26819 17.272 9.51577L10.8763 15.2492L10.6273 17.8883L9.3686 14.0543V14.0543Z" fill="white"></path>
+                                </svg>
+                            </p>
+                        </div>
+                    </div>
+                </Box>
             </aside>
         </div>
     );

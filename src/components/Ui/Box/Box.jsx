d88444cd38f2371983ca25d156cea6f24d055bd1
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-03-27 22:05:15 +0330

    add form and tab in the modal

--- src/components/Ui/Box/Box.jsx
+++ src/components/Ui/Box/Box.jsx
@@ -3,7 +3,7 @@ import "./Box.css"
 
 const Box=(props)=>{
     return(
-        <div className={`box ${props.type&&props.type==="category"?"box-category":''}`}>
+        <div className={`box ${props.type&&props.type==="category"?"box-category":''} ${props.modal?'box--modal':''}`}>
             {props.children}
         </div>
     )
--- src/components/Ui/UploadModal/UploadModal.jsx
+++ src/components/Ui/UploadModal/UploadModal.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./UploadModal.css"
+import "./UploadModal.css";
 import Wrapper from "../../../hoc/Wrapper";
 import Backdrop from "../Backdrop/Backdrop";
 import Box from "../Box/Box";
@@ -12,16 +12,51 @@ const UploadModal = (props) => {
 
             <Backdrop show={props.show} close={props.close} />
 
-            <Box>
-                <div>
-                    
-                </div>
-            </Box>
-            
-                 
+            <section className="modal-upload" >
+                <Box modal={true}>
+                    <div className="nav-btn">
+                        <div className="btn-item">
+                            افزودن با آدرس
+                        </div>
+                        <div className="btn-item">
+                            افزودن فایل
+                        </div>
+                    </div>
+                    {/* image with url */}
+                    <section className="modal-upload--url">
+                        <div className="modal-upload--url__row">
+                            <div className="modal-upload__form">
+                                <div className="form-control">
+                                    <p className="form-control__txt">آدرس عکس</p>
+                                    <input
+                                        className="form-control__input"
+                                        type="url"
+                                    />
+                                </div>
+                                <div className="form-control">
+                                    <p className="form-control__txt">طول عکس</p>
+                                    <input
+                                        className="form-control__input"
+                                        type="url"
+                                    />
+                                </div>
+                                <div className="form-control">
+                                    <p className="form-control__txt">عرض عکس</p>
+                                    <input
+                                        className="form-control__input"
+                                        type="url"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </Box>
+            </section>
+
+
         </Wrapper>
 
     );
 };
 
-export default UploadModal
+export default UploadModal;

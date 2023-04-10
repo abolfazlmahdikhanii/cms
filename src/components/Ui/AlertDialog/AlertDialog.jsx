2ccb16e2770543198fe16541c560426c43699d67
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-04-10 18:15:56 +0330

    modified alert

--- src/components/Ui/AlertDialog/AlertDialog.jsx
+++ src/components/Ui/AlertDialog/AlertDialog.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { toast } from 'react-toastify';
 import "./AlertDialog.css";
 import Wrapper from "../../../hoc/Wrapper";
 import Backdrop from "../Backdrop/Backdrop";
@@ -6,27 +7,43 @@ import Box from "../Box/Box";
 import { supabase } from "../../../superbase";
 
 
-const AlertDialog = (props) => {
-    const [blogId,setBlogId]=useState(props?.id)
+const AlertDialog = ({id,close,session,show}) => {
+    const [blogId,setBlogId]=useState(id)
+    
+    const toastOption={
+        position: "bottom-right",
+        autoClose:1000,
+         hideProgressBar: true,
+         theme:"colored",
+         style:{fontFamily:"shabnam,sans-serif"}
+    }
+    
    useEffect(()=>{
-console.log(props.id);
 
-setBlogId(props?.id)
-   },[props?.id,blogId])
+
+setBlogId(id)
+   },[id,blogId])
     
     const removeBlogHandler=async(id)=>{
         try{
-            const {user}=props?.session
+        
+            const {user}=session
             const {err}=await supabase.from("blogs").delete()
             .eq("id",id)
             .eq("post_author",user?.id)
             if(err) throw err
+            toast.success("پست شما با موفقیت حذف شد ",toastOption)
 
-            props.close()
+            close()
         }
         catch(err){
-            console.log(err);
+         
+            toast.error("حذف پست شما با مشکل مواجه شد",toastOption)
+
             
+        }
+        finally{
+        
         }
     }
   
@@ -34,16 +51,16 @@ setBlogId(props?.id)
         <Wrapper>
 
 
-            <Backdrop show={props.show} />
+            <Backdrop show={show} />
 
-            <section className={`alert-dialog ${!props.show ? 'alert-dialog--hidden' : ''}`} >
+            <section className={`alert-dialog ${!show ? 'alert-dialog--hidden' : ''}`} >
                 <Box modal={true} >
                   <h4 className="dialog-title__title">حذف پست</h4>
                   <div className="dialog-body">
                     <p className="dialog-dis__dis">آیا از پاک کردن این پیش‌نویس مطمئن هستید؟ در صورت حذف، پیش‌نویس شما قابل بازیابی نخواهد بود.</p>
 
                     <div className="dialog-btns">
-                        <button className="btn-item outline-btn outline-btn--cancel" onClick={props.close}>منصرف شدم</button>
+                        <button className="btn-item outline-btn outline-btn--cancel" onClick={close}>منصرف شدم</button>
                         <button className="btn-item outline-btn outline-btn--remove" onClick={()=>removeBlogHandler(blogId)}>بله، حذف شود</button>
 
                     </div>

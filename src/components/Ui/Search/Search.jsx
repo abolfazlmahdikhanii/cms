97a774a0164109b09fe5c9ad864c72365a5d6ebb
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-04-12 22:05:51 +0330

    modified search

--- src/components/Ui/Search/Search.jsx
+++ src/components/Ui/Search/Search.jsx
@@ -6,7 +6,7 @@ const Search = ({disable,setClickInput,search,searchHandler}) => {
 
     return (
         <div className="search-wrapper">
-        <div className={`form-control--search ${disable?'form-control--search__active':""}`} onClick={()=>setClickInput(true)}  onBlur={()=>setClickInput(false)}>
+        <div className={`form-control--search ${disable?'form-control--search__active':""}`} onClick={()=>setClickInput(true)}  >
             <div className="form-control--search__icon">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" width={18} height={18}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
@@ -15,11 +15,18 @@ const Search = ({disable,setClickInput,search,searchHandler}) => {
             </div>
             <input type="text" className="form-control__input" placeholder="جستجو"
              value={search}
-            disabled={!disable} />
+            disabled={!disable} 
+            onChange={(e)=>searchHandler(e.target.value)}
+            
+            />
 
   
         </div>
-        <SerachModal show={disable}/>
+        <SerachModal show={disable} close={()=>{
+            setClickInput(false)
+            searchHandler("")
+        
+        }}/>
         </div>
     );
 };

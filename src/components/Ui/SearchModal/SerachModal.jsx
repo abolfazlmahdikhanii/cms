459479d927f306c1a81078cdd5bb8eb0d3000500
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-04-13 20:07:17 +0330

    modified search modal

--- src/components/Ui/SearchModal/SerachModal.jsx
+++ src/components/Ui/SearchModal/SerachModal.jsx
@@ -3,14 +3,24 @@ import "./SearchModal.css";
 import Wrapper from "../../../hoc/Wrapper";
 import Backdrop from "../Backdrop/Backdrop";
 import Box from "../Box/Box";
+import ItemList from "../ItemList/ItemList";
 
-export const SerachModal = ({ show,close }) => {
+export const SerachModal = ({ show, close, findBlog }) => {
     return (
         <Wrapper>
             <Backdrop show={show} close={close} />
             <section className={`search-dialog ${show ? "search-dialog--hidden" : ""}`} >
                 <div className="search-dialog--box">
-
+                    {
+                        findBlog?.map((item) => {
+                            return (
+                                <ItemList
+                                    key={item?.id}
+                                    title={item?.title}
+                                />
+                            );
+                        })
+                    }
                 </div>
             </section>
         </Wrapper>

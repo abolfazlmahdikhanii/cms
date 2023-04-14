66df1a385189c1b2b922f60a7d9ffa4f3c5b86f7
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-04-14 20:40:26 +0330

    modified

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -7,100 +7,102 @@ import { Route, Router, Routes } from "react-router-dom";
 import Post from "../../components/Post/Post";
 import Layout from "./Layout";
 import { supabase } from "../../superbase";
-import logo from "../../assets/logo.svg"
+import logo from "../../assets/logo.svg";
 
 
-const Home = ({session}) => {
-    const[firstName,setFirstName]=useState("")
-    const[lastName,setLastName]=useState("")
-    const[clickInput,setClickInput]=useState(false)
-    const[search,setSearch]=useState("")
-    const[findBlog,setFindBlog]=useState([])
 
-    const[avatarUrl,setAvatarUrl]=useState(null)
+const Home = ({ session }) => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [clickInput, setClickInput] = useState(false);
+  const [search, setSearch] = useState("");
+  const [findBlog, setFindBlog] = useState(null);
 
-    
-    useEffect(()=>{
-
-      showUserName()
-    },[session])
-
-    const showUserName = async () => {
+  const [avatarUrl, setAvatarUrl] = useState(null);
 
 
-        try {
+  useEffect(() => {
+
+    showUserName();
+  }, [session]);
+
+  const showUserName = async () => {
+
+
+    try {
+
+      let { data, error } = await supabase
+        .from("profiles")
+        .select(`firstName,lastName,avatar_url`)
+        .eq('id', session?.user?.id)
+        .single();
+
+
+      if (error) throw error;
+
+
+
+
+
+
+      setFirstName(data?.firstName);
+      setLastName(data?.lastName);
+      setAvatarUrl(data?.avatar_url);
+
+    }
+    catch (error) {
+
+      console.log(error);
+
+    }
+
+  };
+  const searchInputHandler = async (e) => {
+    try {
+      setSearch(e.target.value);
+
+      const { data, error } = await supabase.from("blogs")
+        .select("id,post_title,post_content,post_type,post_author(username)")
+        .ilike("post_title", `%${search}%`)
         
-            let { data, error } = await supabase
-                .from("profiles")
-                .select(`firstName,lastName,avatar_url`)
-                .eq('id',session?.user?.id)
-                .single();
+      if (error) throw error;
 
-
-            if (error)  throw error;
-            
-          
-               
-                
- 
-
-                setFirstName(data?.firstName);
-                setLastName(data?.lastName);
-                setAvatarUrl(data?.avatar_url);
-            
-        }
-        catch (error) {
-          
-            console.log(error);
-
-        }
-
-    };
-   const searchInputHandler=async(e)=>{
-    try{
-      setSearch(e.target.value)
-
-      const {data,error}=await supabase.from("blogs")
-      .select("*")
-      .ilike("post_title",`%${search}%`)
-      if(error)throw error
-
-      setFindBlog(data)
+      setFindBlog(data);
       console.log(data);
-      
 
-    }catch(err){
+
+    } catch (err) {
       console.log(err);
-      
-    }
-      
+
     }
 
-   
+  };
 
 
-    return (
-        <Wrapper>
-            <Header fullName={`${firstName} ${lastName}`}>
 
-              <img src={logo} alt="" />
-                <Search 
-                  disable={clickInput}
-                  setClickInput={setClickInput}
-                  search={search}
-                  searchHandler={searchInputHandler}
-                  findBlog={findBlog}
-                />
-                
-            </Header>
-      
+
+  return (
+    <Wrapper>
+      <Header fullName={`${firstName} ${lastName}`}>
+
+        <img src={logo} alt="" />
+        <Search
+          disable={clickInput}
+          setClickInput={setClickInput}
+          search={search}
+          searchHandler={searchInputHandler}
+          findBlog={findBlog}
+        />
+     
+      </Header>
+
       <Routes>
-        <Route path="/" element={<Layout/>}/>
-        <Route path="/article/:username/:id/:title" element={<Post session={session}/>}/>
+        <Route path="/" element={<Layout />} />
+        <Route path="/article/:username/:id/:title" element={<Post session={session} />} />
       </Routes>
-      
-        </Wrapper>
-    );
+
+    </Wrapper>
+  );
 };
 
 export default Home;

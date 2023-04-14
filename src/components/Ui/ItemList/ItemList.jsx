af7b6d3aa1acc502ef406b31dda9da6b2d028ec7
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-04-14 20:39:41 +0330

    modified item list

--- src/components/Ui/ItemList/ItemList.jsx
+++ src/components/Ui/ItemList/ItemList.jsx
@@ -1,18 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import useFilterImage from "../../../hooks/useFilterImage";
+import useFilterPargraph from "../../../hooks/useFilterParagraph";
 
-const ItemList = ({img,title}) => {
-  return (
-    <div className="aside-item">
-    <div className="aside-item--img">
-        <img src="../../../src/assets/bg-slider.jpg" alt="" className="aside-item--img__img" />
-    </div>
-    <div className="aside-item--title">
-        <h3 className="aside-item--title__title">        
-          {title}
-        </h3>
-    </div>
-</div>
-  );
+
+const ItemList = ({ title,id,username,content }) => {
+
+    const filterImg = useFilterImage;
+    const filterParagraph = useFilterPargraph;
+
+
+    return (
+        <Link className="aside-item" to={`/article/@${username}/${id}/${title.split(" ").join("-")}`}>
+            <div className="aside-item--img" dangerouslySetInnerHTML={{ __html: filterImg(content) }}>
+
+            </div>
+            <div className="aside-item--title">
+                <h3 className="search-item--title__title">
+                    {title}
+                </h3>
+
+
+            </div>
+        </Link>
+    );
 };
 
 export default ItemList;

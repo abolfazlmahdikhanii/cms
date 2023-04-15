cb2f1d965c202500f9805d7c5c17dbb115170fef
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-04-15 11:02:21 +0330

    modified search modal

--- src/components/Ui/SearchModal/SerachModal.jsx
+++ src/components/Ui/SearchModal/SerachModal.jsx
@@ -4,17 +4,32 @@ import Wrapper from "../../../hoc/Wrapper";
 import Backdrop from "../Backdrop/Backdrop";
 import Box from "../Box/Box";
 import ItemList from "../ItemList/ItemList";
+import { Oval } from 'react-loader-spinner';
 
-const SerachModal = ({ show, close, findBlog }) => {
+const SerachModal = ({ show, close, findBlog,loading }) => {
     return (
         <Wrapper>
+
             <Backdrop show={show} close={close} />
             <section className={`search-dialog ${show ? "search-dialog--hidden" : ""}`} >
+                <Oval
+                    height={30}
+                    width={30}
+                    color="#4338ca"
+                    wrapperStyle={{}}
+                    wrapperClass="oval-loader"
+                    visible={loading}
+                    ariaLabel='oval-loading'
+                    secondaryColor="#818cf8"
+                    strokeWidth={6}
+                    strokeWidthSecondary={6}
+
+                />
                 <div className="search-dialog--box">
                     {
-                        findBlog?.flatMap(( item) => {
-                      
-                            
+                        findBlog?.flatMap((item) => {
+
+
                             return (
                                 <ItemList
                                     key={item?.id}

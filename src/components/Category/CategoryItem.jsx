cdf91e3dd9700020897f532310a05b406d08ed2f
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-04-17 20:33:47 +0330

    remove link

--- src/components/Category/CategoryItem.jsx
+++ src/components/Category/CategoryItem.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./CategoryItem.css";
-import {  NavLink } from "react-router-dom";
+
 const CategoryItem = ({icon,title,bgIcon,bgItem,value,category,setCategory}) => {
     return (
-        <NavLink end to={`/category/${value==="last-news"?"":value}`} className="category-item" style={{backgroundColor:bgItem}} onClick={()=>setCategory(value)}>
+        <div className="category-item" style={{backgroundColor:bgItem}}>
             <div className="category-item--icon" style={{backgroundColor:bgIcon}}>
                 <p>
                     <img src={icon} alt="icon-category" />
@@ -14,7 +14,7 @@ const CategoryItem = ({icon,title,bgIcon,bgItem,value,category,setCategory}) => 
             <div>
                 <h4 className="category-item--title">{title}</h4>
             </div>
-        </NavLink>
+        </div>
     );
 };
 

85dac06268f32ee02830c9a041b7e8c1b8eb6d52
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-04-20 18:58:12 +0330

    create hook for get pulicUrl

---
+++ src/hooks/usePublicProfile.jsx
@@ -0,0 +1,26 @@
+import React,{useEffect,useState} from "react";
+import { supabase } from "../superbase";
+const usePublicProfile = (location) => {
+
+    const [url,setUrl]=useState("")
+    useEffect(() => {
+         downloadImage(location);
+    }, [location]);
+
+    const downloadImage = async (path) => {
+
+        try {
+            const { data, error } =  supabase.storage.from('avatars').getPublicUrl(path);
+            if (error) throw error;
+            setUrl(data.publicUrl)
+        
+        }
+        catch (error) {
+            console.log(error.message);
+        }
+    };
+
+    return url
+};
+
+export default usePublicProfile;

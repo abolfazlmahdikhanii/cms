1b972a52ddd1f98ab5a476fac7c3677b738a12a3
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-04-21 12:52:31 +0330

    modifie create blog

--- src/containers/CmsPanel/CreateBlog/CreateBlog.jsx
+++ src/containers/CmsPanel/CreateBlog/CreateBlog.jsx
@@ -519,8 +519,8 @@ const CreateBlog = (props) => {
                                     <option value="life-style">سبک زندگی</option>
                                     <option value="game">بازی و سرگرمی</option>
                                 </select>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width={24} height={24}>
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" width={24} height={24}>
+                                    <path strokeLinecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                                 </svg>
                             </div>
 

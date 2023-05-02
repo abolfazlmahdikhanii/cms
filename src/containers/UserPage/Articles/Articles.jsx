78987b74a352196df7c570accba14de9f89f8585
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-05-02 20:45:59 +0330

    create articles component

---
+++ src/containers/UserPage/Articles/Articles.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Articles.css";
+import PostCard from "../../../components/Ui/PostCard/PostCard"
+
+const Articles = () => {
+  return (
+    <div>
+      <div className="blog-list--title">
+        <div className="blog-list--title__icon">
+          <svg width="37" height="34" viewBox="0 0 37 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="10" cy="24" r="10" fill="currentColor"></circle>
+            <circle cx="30" cy="13" r="7" fill="currentColor" fillOpacity="0.4"></circle>
+            <circle cx="15" cy="4" r="4" fill="currentColor" fillOpacity="0.7"></circle>
+          </svg>
+        </div>
+        <div>
+          <h3 className="blog-list--title__txt">مقالات</h3>
+        </div>
+      </div>
+
+      <div>
+      <PostCard/>
+      </div>
+
+    </div>
+
+  );
+};
+
+export default Articles;

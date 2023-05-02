92fa1c5e219229c3346cdb94721eddf3aae71b58
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-05-02 20:34:09 +0330

    add user page component

---
+++ src/containers/UserPage/AboutUser/AboutUser.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./AoutUser.css"
+import Box from "../../../components/Ui/Box/Box";
+
+const AboutUser = () => {
+  return(
+ <Box>
+    <div className="about-user--wrapper">
+        <h4 className="about-user__title">درباره من</h4>
+        <p className="about-user__dis">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae sapiente, corporis reprehenderit iure fugiat placeat, optio minima repudiandae labore ipsa omnis recusandae explicabo ab quia sequi quisquam corrupti eum tempora!</p>
+    </div>
+ </Box>
+  );
+};
+
+export default AboutUser;

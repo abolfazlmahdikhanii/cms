bc7071cb66bf0dcf5ae7277179e2d3a2c2cf5878
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-05-03 16:36:30 +0330

    set data

--- src/containers/UserPage/AboutUser/AboutUser.jsx
+++ src/containers/UserPage/AboutUser/AboutUser.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./AoutUser.css"
 import Box from "../../../components/Ui/Box/Box";
 
-const AboutUser = () => {
+const AboutUser = ({about}) => {
   return(
  <Box>
     <div className="about-user--wrapper">
         <h4 className="about-user__title">درباره من</h4>
-        <p className="about-user__dis">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae sapiente, corporis reprehenderit iure fugiat placeat, optio minima repudiandae labore ipsa omnis recusandae explicabo ab quia sequi quisquam corrupti eum tempora!</p>
+        <p className="about-user__dis">
+          {about}
+        </p>
     </div>
  </Box>
   );

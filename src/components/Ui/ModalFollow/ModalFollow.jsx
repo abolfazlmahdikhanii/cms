0b7c7f8f3fc7d9569c347f5e585a098539c99235
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-05-07 13:07:20 +0330

    modified

--- src/components/Ui/ModalFollow/ModalFollow.jsx
+++ src/components/Ui/ModalFollow/ModalFollow.jsx
@@ -1,25 +1,45 @@
 import React from "react";
-import "./ModalFollow.css"
+import "./ModalFollow.css";
 import Box from "../Box/Box";
 import Wrapper from "../../../hoc/Wrapper";
-import Backdrop from "../../Ui/Backdrop/Backdrop"
+import Backdrop from "../../Ui/Backdrop/Backdrop";
+import FollowItem from "./FollowItem";
 
-const ModalFollow = () => {
+const ModalFollow = ({ followes, show, activeTab, close, setActiveTab }) => {
+
+
     return (
         <Wrapper>
 
-            <Backdrop  />
+            <Backdrop show={show} close={close}/>
 
-            <section className="modal-follow">
+            <section className={`modal-follow ${!show ? "modal-follow--hidden" : ""}`}>
                 <Box modal={true}>
-                 <div className="row follow-nav">
-                    <div className="blog-list__tab">
-                        دنبال کننده ها
+                    <div className="row follow-nav">
+                        <div className={`blog-list__tab ${activeTab === "follower" ? "active" : ""}`} onClick={() => setActiveTab("follower")}>
+                            دنبال کننده ها
+                        </div>
+                        <div className={`blog-list__tab ${activeTab === "following" ? "active" : ""}`} onClick={() => setActiveTab("following")}>
+                            دنبال شده ها
+                        </div>
                     </div>
-                    <div className="blog-list__tab">
-                        دنبال شده ها
+                    <div className="follow-body">
+                        {
+
+                            followes?.map((item) => {
+                                const { id } = item?.user_follower || item?.user_follow;
+
+                            
+
+                                return (
+                                    < FollowItem
+                                        key={id}
+                                        {...item?.user_follower || item?.user_follow}
+                                    />
+                                );
+                            })
+                        }
                     </div>
-                 </div>
                 </Box>
             </section>
         </Wrapper>

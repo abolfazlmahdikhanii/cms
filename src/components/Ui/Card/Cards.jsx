6540c8e426ca4691da3d6573214e5b7693eecfda
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-05-22 13:34:38 +0330

    responsive

--- src/components/Ui/Card/Cards.jsx
+++ src/components/Ui/Card/Cards.jsx
@@ -1,54 +1,56 @@
 import React from "react";
 import Box from "../Box/Box";
 import CardItem from "./CardItem";
-import "./Cards.css"
-import blogs from "../../../assets/blogs.svg"
-import blogReject from "../../../assets/blogs-reject.svg"
-import star from "../../../assets/star.svg"
+import "./Cards.css";
+import blogs from "../../../assets/blogs.svg";
+import blogReject from "../../../assets/blogs-reject.svg";
+import star from "../../../assets/star.svg";
 
 
-const Cards=(props)=>{
+const Cards = (props) => {
 
-    const info=[
+    const info = [
         {
-            icon:blogs,
-            title:"مقالات منتشر شده",
-            subTitle:"20 مقاله",
-            iconColor:"#3b82f6"
+            icon: blogs,
+            title: "مقالات منتشر شده",
+            subTitle: "20 مقاله",
+            iconColor: "#3b82f6"
         },
         {
-            icon:blogReject,
-            title:"مقالات منتشر نشده",
-            subTitle:"20 مقاله",
-            iconColor:"#ef4444"
+            icon: blogReject,
+            title: "مقالات منتشر نشده",
+            subTitle: "20 مقاله",
+            iconColor: "#ef4444"
         },
         {
-            icon:star,
-            title:"امتیازات",
-            subTitle:"10 ستاره",
-            iconColor:"#f59e0b"
+            icon: star,
+            title: "امتیازات",
+            subTitle: "10 ستاره",
+            iconColor: "#f59e0b"
         }
-    ]
-    return(
-        
-       <Box>
-         <div className="cards">
+    ];
+    return (
+
+
+        <div className="cards">
             {
-                info.map((item,i)=>{
-                    return(
-                        <CardItem
-                        key={i}
-                        {...item}
-                        />
-                    )
+                info.map((item, i) => {
+                    return (
+                        <Box key={i}>
+                            <CardItem
+
+                                {...item}
+                            />
+                        </Box>
+                    );
                 })
             }
 
-            
+
         </div>
 
-       </Box>
-    )
-}
 
-export default Cards
+    );
+};
+
+export default Cards;

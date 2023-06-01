86715b7cbb4ccb56e8d37ae304b203d32813bdac
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-06-01 15:15:52 +0330

    modified

--- src/components/TypographyBtn/TypographyBtn.jsx
+++ src/components/TypographyBtn/TypographyBtn.jsx
@@ -2,19 +2,21 @@ import React from "react";
 
 const TypographyBtn = ({ show, changeHeading, close }) => {
     const headings = ["h1", "h2", "h3", "h4", "h5", "h6"];
-    console.log(show);
-    
+  
+
     return (
         <div className={`drop-down--typo ${show ? 'show-drowp-down--2' : ''}`}>
             <ul className="drowp-down-status__list--2">
                 {
                     headings.map((item, i) => {
+                        let Tag = item;
                         return (
+
                             <li key={i} className="profile-item drowp-down__txt--2" onClick={() => {
                                 changeHeading(item);
                                 close();
                             }}>
-                                {item}
+                                <Tag>{item} </Tag>
                             </li>
                         );
                     })

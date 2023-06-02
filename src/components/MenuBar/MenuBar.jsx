74286691c91261bc7bb25db997ed59e854a8d0b7
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-06-02 15:13:30 +0330

    modified

--- src/components/MenuBar/MenuBar.jsx
+++ src/components/MenuBar/MenuBar.jsx
@@ -15,15 +15,15 @@ const MenuBar = ({ changeHandler }) => {
         },
         {
             title: "em",
-            icon: <FaItalic size={16}/>
+            icon: <FaItalic size={16} />
         },
         {
             title: "ins",
-            icon: <FaUnderline size={16}/>
+            icon: <FaUnderline size={16} />
         },
         {
             title: "a",
-            icon: <FaLink size={16}/>
+            icon: <FaLink size={16} />
         },
         {
             title: "img",
@@ -31,34 +31,34 @@ const MenuBar = ({ changeHandler }) => {
         },
         {
             title: "center",
-            icon: <FaAlignCenter size={16}/>
+            icon: <FaAlignCenter size={16} />
         },
         {
             title: "right",
-            icon: <FaAlignRight size={16}/>
+            icon: <FaAlignRight size={16} />
         },
         {
             title: "left",
-            icon: <FaAlignLeft size={16}/>
+            icon: <FaAlignLeft size={16} />
         },
         {
             title: "ul",
-            icon: <FaListUl size={16}/>
+            icon: <FaListUl size={16} />
         },
         {
             title: "ol",
-            icon: <FaListOl size={16}/>
+            icon: <FaListOl size={16} />
         },
         {
             title: "quote",
-            icon: <FaQuoteRight size={16}/>
+            icon: <FaQuoteRight size={16} />
         },
     ];
 
 
     return (
         <>
-        
+
             {
                 btns.map((item, i) => {
                     return (
@@ -69,12 +69,12 @@ const MenuBar = ({ changeHandler }) => {
                 })
             }
 
-    {/* heading */}
-    <div className=" btn-item">
+            {/* heading */}
+            <div className=" btn-item">
                 <div className=""
                     onClick={() => changeHandler(changeHeading)}
                 >
-                    <p style={{textTransform:"uppercase"}} className="btn-item__txt" >{changeHeading}</p>
+                    <p style={{ textTransform: "uppercase" }} className="btn-item__txt" >{changeHeading}</p>
                 </div>
                 <p className="btn-status__svg" onClick={() => setShow(!show)}>
 

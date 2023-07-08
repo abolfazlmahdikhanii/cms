4575cf4f6e6078019f3a9d2c7156ea5408c9fb80
abolfazlmahdikhanii <abolfazlmahdikhanii@gmail.com>
2023-07-08 21:05:25 +0330

    modified

--- src/hooks/usePublicProfile.jsx
+++ src/hooks/usePublicProfile.jsx
@@ -1,5 +1,6 @@
 import React,{useEffect,useState} from "react";
 import { supabase } from "../superbase";
+import profileIcon from "../../src/assets/profile.svg"
 const usePublicProfile = (location) => {
 
     const [url,setUrl]=useState("")
@@ -10,9 +11,14 @@ const usePublicProfile = (location) => {
     const downloadImage = async (path) => {
 
         try {
+          if(path){
             const { data, error } =  supabase.storage.from('avatars').getPublicUrl(path);
             if (error) throw error;
-            setUrl(data.publicUrl)
+            setUrl(data?.publicUrl)
+          }
+          else{
+            setUrl(profileIcon)
+          }
         
         }
         catch (error) {
